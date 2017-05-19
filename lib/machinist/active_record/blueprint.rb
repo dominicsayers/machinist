e19348c1abfcd89a8d9ae957052a1c0667ef25a9
Dominic Sayers <dominic@sayers.cc>
2017-05-19 16:31:27 +0100

    Better diagnostics when ActiveRecord model can't be instantiated. Fixes #4

--- lib/machinist/active_record/blueprint.rb
+++ lib/machinist/active_record/blueprint.rb
@@ -1,14 +1,19 @@
-module Machinist::ActiveRecord
-  class Blueprint < Machinist::Blueprint
-    # Make and save an object.
-    def make!(attributes = {})
-      object = make(attributes)
-      object.save!
-      object.reload
-    end
+module Machinist
+  module ActiveRecord
+    class Blueprint < Machinist::Blueprint
+      # Make and save an object.
+      def make!(attributes = {})
+        object = make(attributes)
+        object.save!
+        object.reload
+      rescue ::ActiveRecord::RecordInvalid => exception
+        message = "#{exception.class.name}: \"#{exception.message}\" saving #{exception.record.class.name}"
+        raise Machinist::BlueprintCantSaveError.new(self, message)
+      end
 
-    def lathe_class #:nodoc:
-      Machinist::ActiveRecord::Lathe
+      def lathe_class #:nodoc:
+        Machinist::ActiveRecord::Lathe
+      end
     end
   end
 end
